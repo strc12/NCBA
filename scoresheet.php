b2292734d6a513d65a47764f1419b14dea3d5fe0
strc12 <ric@oundleschool.org.uk>
2024-06-19 12:54:55 +0100

    start to update scores every time

--- scoresheet.php
+++ scoresheet.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-print_r($_POST);
-print_r($_SESSION);
+//print_r($_POST);
+//print_r($_SESSION);
 
 include_once ("connection.php");
 // Check if the form is submitted to update the item
 
-    $sql = "UPDATE tblmatches SET HomeP1ID = :p1h, HomeP2ID = :p2h, HomeP3ID = :p3h, HomeP4ID = :p4h, HomeP5ID = :p5h, HomeP6ID=:p6h,
+    $sql = "UPDATE TblMatches SET HomeP1ID = :p1h, HomeP2ID = :p2h, HomeP3ID = :p3h, HomeP4ID = :p4h, HomeP5ID = :p5h, HomeP6ID=:p6h,
     AwayP1ID = :p1a, AwayP2ID = :p2a, AwayP3ID = :p3a, AwayP4ID = :p4a, AwayP5ID = :p5a, AwayP6ID=:p6a
      WHERE MatchID = :id";
     $stmt = $conn->prepare($sql);
@@ -36,35 +36,35 @@ include_once ("connection.php");
 ?>
 <?php
 include_once ("connection.php");
-$stmt=$conn->prepare("SELECT tblmatches.FixtureDate, 
-tblmatches.m1h,tblmatches.m1a,
-tblmatches.m2h,tblmatches.m2a,
-tblmatches.m3h,tblmatches.m3a,
-tblmatches.m4h,tblmatches.m4a,
-tblmatches.m5h,tblmatches.m5a,
-tblmatches.m6h,tblmatches.m6a,
-tblmatches.m7h,tblmatches.m7a,
-tblmatches.m8h,tblmatches.m8a,
-tblmatches.m9h,tblmatches.m9a,
-tblmatches.m10h,tblmatches.m10a,
-tblmatches.m11h,tblmatches.m11a,
-tblmatches.m12h,tblmatches.m12a,
-tblmatches.m13h,tblmatches.m13a,
-tblmatches.m14h,tblmatches.m14a,
-tblmatches.m15h,tblmatches.m15a,
-tblmatches.m16h,tblmatches.m16a,
-tblmatches.m17h,tblmatches.m17a,
-tblmatches.m18h,tblmatches.m18a,
-tblmatches.m19h,tblmatches.m19a,
-tblmatches.m20h,tblmatches.m20a,
-tblmatches.m21h,tblmatches.m21a,
-tblmatches.m22h,tblmatches.m22a,
-tblmatches.m23h,tblmatches.m23a,
-tblmatches.m24h,tblmatches.m24a,
-tblmatches.m25h,tblmatches.m25a,
-tblmatches.m26h,tblmatches.m26a,
-tblmatches.m27h,tblmatches.m27a,
-tblmatches.HomeID as Home, tblmatches.AwayID as Away,  
+$stmt=$conn->prepare("SELECT TblMatches.FixtureDate, 
+TblMatches.m1h,TblMatches.m1a,
+TblMatches.m2h,TblMatches.m2a,
+TblMatches.m3h,TblMatches.m3a,
+TblMatches.m4h,TblMatches.m4a,
+TblMatches.m5h,TblMatches.m5a,
+TblMatches.m6h,TblMatches.m6a,
+TblMatches.m7h,TblMatches.m7a,
+TblMatches.m8h,TblMatches.m8a,
+TblMatches.m9h,TblMatches.m9a,
+TblMatches.m10h,TblMatches.m10a,
+TblMatches.m11h,TblMatches.m11a,
+TblMatches.m12h,TblMatches.m12a,
+TblMatches.m13h,TblMatches.m13a,
+TblMatches.m14h,TblMatches.m14a,
+TblMatches.m15h,TblMatches.m15a,
+TblMatches.m16h,TblMatches.m16a,
+TblMatches.m17h,TblMatches.m17a,
+TblMatches.m18h,TblMatches.m18a,
+TblMatches.m19h,TblMatches.m19a,
+TblMatches.m20h,TblMatches.m20a,
+TblMatches.m21h,TblMatches.m21a,
+TblMatches.m22h,TblMatches.m22a,
+TblMatches.m23h,TblMatches.m23a,
+TblMatches.m24h,TblMatches.m24a,
+TblMatches.m25h,TblMatches.m25a,
+TblMatches.m26h,TblMatches.m26a,
+TblMatches.m27h,TblMatches.m27a,
+TblMatches.HomeID as Home, TblMatches.AwayID as Away,  
 P1.Forename as P1f, P1.Surname as P1s, 
 P2.Forename as P2f, P2.Surname as P2s,
 P3.Forename as P3f, P3.Surname as P3s,
@@ -102,9 +102,9 @@ $stmt->bindParam(':id', $_SESSION['curmatch']);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-print_r($row);
+//print_r($row);
 echo("<br>");
-print_r($_SESSION);
+//print_r($_SESSION);
 
 ?>
 <!DOCTYPE HTML>
@@ -119,7 +119,7 @@ print_r($_SESSION);
   <link href="styles.css" rel="stylesheet">
   <script>
         // removes session variable counter
-    sessionStorage.removeItem("counter");
+    //sessionStorage.removeItem("counter");
     
     /* function totalscores(){
         var homegamestotal=0;
@@ -152,7 +152,7 @@ print_r($_SESSION);
         sessionStorage.clear();
     } */
 
-    function checkfilled(){
+   /*  function checkfilled(){
     //alert (document.getElementById('m1h1').innerHTML);
         if (document.getElementById('m1a1').innerText=='' || document.getElementById('m2a1').innerText=='' 
         || document.getElementById('m3a1').innerText=='' || document.getElementById('m3aa1').innerText=='' 
@@ -171,7 +171,7 @@ print_r($_SESSION);
         
             return 0;
         }
-    }
+    } */
 function prepopulate(id){
     //fills in values if already entered and stored in the session variables
     //console.log(Object.keys(sessionStorage));
@@ -189,9 +189,9 @@ function prepopulate(id){
 function prepopres(id){
     //console.log(Object.keys(sessionStorage));
     //console.log(Object(sessionStorage));
-    if (sessionStorage.counter==null){
+    /* if (sessionStorage.counter==null){
     sessionStorage.counter=1;
-    }
+    } */
     //console.log(id);
     if(sessionStorage.getItem(id)) {
     document.getElementById(id).innerText = sessionStorage.getItem(id);
@@ -201,7 +201,7 @@ function prepopres(id){
     document.getElementById(id).addEventListener('input', function() {
     sessionStorage.setItem(id, this.innerText);
     });  
-    sessionStorage.counter=Number(sessionStorage.counter)+1;
+    //sessionStorage.counter=Number(sessionStorage.counter)+1;
     
 }
 function games(match1, match2, home, away, box) {
@@ -300,22 +300,59 @@ function games(match1, match2, home, away, box) {
         document.getElementById("but").style.display = 'none';
     }  */   
 function totals(){
-        rubbers = [
-     ['m1hr', 'm2hr', 'm3hr', 'm4hr', 'm5hr', 'm6hr', 'm7hr', 'm8hr','m9hr','m10hr','m11hr','m12hr','m13hr','m14hr','m15hr','m16hr','m17hr','m18hr','m19hr','m20hr','m21hr','m22hr','m23hr','m24hr','m25hr','m26hr','m27hr'],
-     ['m1ar', 'm2ar', 'm3ar', 'm4ar', 'm5ar', 'm6ar', 'm7ar', 'm8ar','m9ar','m10ar','m11ar','m12ar','m13ar','m14ar','m15ar','m16ar','m17ar','m18ar','m19ar','m20ar','m21ar','m22ar','m23ar','m24ar','m25ar','m26ar','m27ar']
-        ];
+    rubbers = [
+        ['m1hr', 'm2hr', 'm3hr', 'm4hr', 'm5hr', 'm6hr', 'm7hr', 'm8hr','m9hr','m10hr','m11hr','m12hr','m13hr','m14hr','m15hr','m16hr','m17hr','m18hr','m19hr','m20hr','m21hr','m22hr','m23hr','m24hr','m25hr','m26hr','m27hr'],
+        ['m1ar', 'm2ar', 'm3ar', 'm4ar', 'm5ar', 'm6ar', 'm7ar', 'm8ar','m9ar','m10ar','m11ar','m12ar','m13ar','m14ar','m15ar','m16ar','m17ar','m18ar','m19ar','m20ar','m21ar','m22ar','m23ar','m24ar','m25ar','m26ar','m27ar']
+    ];
     game=[
         ['m1hg', 'm4hg', 'm7hg', 'm10hg', 'm13hg', 'm16hg', 'm19hg', 'm22hg', 'm25hg'],
         ['m1ag', 'm4ag', 'm7ag', 'm10ag', 'm13ag', 'm16ag', 'm19ag', 'm22ag', 'm25ag']
-   ];
-   points=[
-    ['m1hpts', 'm2hpts', 'm3hpts', 'm4hpts', 'm5hpts', 'm6hpts', 'm7hpts', 'm8hpts','m9hpts','m10hpts','m11hpts','m12hpts','m13hpts','m14hpts','m15hpts','m16hpts','m17hpts','m18hpts','m19hpts','m20hpts','m21hpts','m22hpts','m23hpts','m24hpts','m25hpts','m26hpts','m27hpts'],
-    ['m1apts', 'm2apts', 'm3apts', 'm4apts', 'm5apts', 'm6apts', 'm7apts', 'm8apts','m9apts','m10apts','m11apts','m12apts','m13apts','m14apts','m15apts','m16apts','m17apts','m18apts','m19apts','m20apts','m21apts','m22apts','m23apts','m24apts','m25apts','m26apts','m27apts']
-   ];
-   let hrtot=0;
-   let artot=0;
-   for (k = 0;k<=rubbers[0].length; k++){
+    ];
+    points=[
+        ['m1hpts', 'm2hpts', 'm3hpts', 'm4hpts', 'm5hpts', 'm6hpts', 'm7hpts', 'm8hpts','m9hpts','m10hpts','m11hpts','m12hpts','m13hpts','m14hpts','m15hpts','m16hpts','m17hpts','m18hpts','m19hpts','m20hpts','m21hpts','m22hpts','m23hpts','m24hpts','m25hpts','m26hpts','m27hpts'],
+        ['m1apts', 'm2apts', 'm3apts', 'm4apts', 'm5apts', 'm6apts', 'm7apts', 'm8apts','m9apts','m10apts','m11apts','m12apts','m13apts','m14apts','m15apts','m16apts','m17apts','m18apts','m19apts','m20apts','m21apts','m22apts','m23apts','m24apts','m25apts','m26apts','m27apts']
+    ];
+    matches=[
+        ["m1hr","m2hr","m3hr"],
+        ["m1ar","m2ar","m3ar"]
+    ];
+    let hr=0;
+    let ar=0;
+    for (k = 0;k<=points[0].length; k++){
+        if(sessionStorage.getItem(points[0][k])>sessionStorage.getItem(points[1][k])){
+            hr=1;
+            ar=0;
+            let homeRubbers = document.getElementById(matches[0][k]);
+        if (homeRubbers) {
+            homeRubbers.innerText = hr;
+        }
+        let awayRubbers = document.getElementById(matches[1][k]);
+        if (awayRubbers) {
+            awayRubbers.innerText = ar;
+        }
+            
+        }else if(sessionStorage.getItem(points[0][k])<sessionStorage.getItem(points[1][k])){
+            hr=1;
+            ar=0;
+            let homeRubbers = document.getElementById(matches[0][k]);
+        if (homeRubbers) {
+            homeRubbers.innerText = hr;
+        }
+        let awayRubbers = document.getElementById(matches[1][k]);
+        if (awayRubbers) {
+            awayRubbers.innerText = ar;
+        }
+        }
+        
+    }
+    console.log("hr",hr);
+    console.log("ar",ar);
+    let hrtot=0;
+    let artot=0;
+    //rubbers total
+    for (k = 0;k<=rubbers[0].length; k++){
         if(sessionStorage.getItem(rubbers[0][k])!=null){
+            
             hrtot+=parseInt(sessionStorage.getItem(rubbers[0][k]));
         }
         if(sessionStorage.getItem(rubbers[1][k])!=null){
@@ -330,7 +367,7 @@ function totals(){
     if (awayRubbersTotalElement) {
         awayRubbersTotalElement.innerText = artot;
     }
-    
+    //games totals
     let hgtot=0;
     let agtot=0;
     for (k = 0;k<=game[1].length; k++){
@@ -349,6 +386,7 @@ function totals(){
     if (awaygamesTotalElement) {
         awaygamesTotalElement.innerText = agtot;
     }
+    //points totals
     let hptot=0;
     let aptot=0;
     for (k = 0;k<=points[0].length; k++){
@@ -368,6 +406,7 @@ function totals(){
     if (awaypointsTotalElement) {
         awaypointsTotalElement.innerText = aptot;
     }
+    //save for prepop
     sessionStorage.setItem('hptot', hptot);
     sessionStorage.setItem('aptot', aptot);
     sessionStorage.setItem('hrtot', hrtot);
@@ -447,13 +486,21 @@ for ($k = 1;$k<=9; $k++){
         $v=3*$k-2+$j;
         ?>
         <td><input autocomplete="off" id="m<?php echo $v;?>hpts" name="m<?php echo $v;?>hpts" 
+       onchange="totals()" 
+        type="text" ><script>prepopulate("m<?php echo $v;?>hpts");</script>
+        </td>
+        <td><input autocomplete="off" id="m<?php echo $v;?>apts" name="m<?php echo $v;?>apts" 
+        onchange="totals()" 
+        type="text" ><script>prepopulate("m<?php echo $v;?>apts");</script>
+        </td>
+        <!-- <td><input autocomplete="off" id="m<?php echo $v;?>hpts" name="m<?php echo $v;?>hpts" 
         onchange="games(this.id,document.getElementById('m<?php echo $v;?>apts').id,document.getElementById('m<?php echo $v;?>hr').id,document.getElementById('m<?php echo $v;?>ar').id,<?php echo $k;?>)" 
         type="text" ><script>prepopulate("m<?php echo $v;?>hpts");</script>
         </td>
         <td><input autocomplete="off" id="m<?php echo $v;?>apts" name="m<?php echo $v;?>apts" 
         onchange="games(document.getElementById('m<?php echo $v;?>hpts').id,this.id,document.getElementById('m<?php echo $v;?>hr').id,document.getElementById('m<?php echo $v;?>ar').id,<?php echo $k;?>)"
         type="text" ><script>prepopulate("m<?php echo $v;?>apts");</script>
-        </td>
+        </td> -->
         <td id="m<?php echo $v;?>hr"><script>prepopres("m<?php echo $v;?>hr");</script></td>
         <td id="m<?php echo $v;?>ar"><script>prepopres("m<?php echo $v;?>ar");</script></td>
         <?php
